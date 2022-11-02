b204a7f8736b008072cf9cb95ca2d2aaeb58159b
AidanaIbragimova <ibragimova.aidana11@gmail.com>
2022-11-02 18:04:15 +0300

    second commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.geom.Area;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle=new Triangle();
+      triangle.a=3;
+      triangle.b=5;
+      triangle.c=6;
+      triangle.area();
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,14 @@
+public class Triangle {
+
+int a;
+int b;
+int c;
+
+
+public void area(){
+    int summa=a*b*c;
+    System.out.println(a+"*"+b+"*"+"*"+c+"="+summa);
+}
+
+    }
+
